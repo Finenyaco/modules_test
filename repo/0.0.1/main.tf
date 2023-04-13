6f09c1663591e2b8f96349f3cb24d68bfc68e5a2
Sophia Tang <Finenyaco@users.noreply.github.com>
2023-04-13 11:57:04 +0800

    Update main.tf

--- repo/0.0.1/main.tf
+++ repo/0.0.1/main.tf
@@ -1,14 +1,18 @@
 variable "repo_name" {
   description = "Name of the GitHub repository"
+  type        = string
 }
 
 variable "repo_description" {
   description = "Description of the GitHub repository"
+  type        = string
+  default     = "For testing"
 }
 
 variable "repo_private" {
   description = "Whether the GitHub repository should be private or public"
-  default     = false
+  type        = bool
+  default     = true
 }
 
 

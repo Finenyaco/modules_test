8658a96bc74d0235dc238582caa1dc4f58f44fa8
Sophia Tang <Finenyaco@users.noreply.github.com>
2023-04-27 15:32:59 +0800

    Update main.tf

--- repo/0.0.3/main.tf
+++ repo/0.0.3/main.tf
@@ -1,8 +1,9 @@
 resource "github_repository" "repo" {
-  name        = var.repo_name
-  description = var.repo_description + var.test_options
-  private     = var.repo_private
+  name             = var.repo_name
+  description      = var.repo_description
+  private          = var.repo_private
   system_disk_size = var.system_disk_size
+  test_options     = var.test_options
   test_list        = var.test_list
   test_mix         = var.test_mix
   test_mix_number  = var.test_mix_number

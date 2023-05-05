d61f554a49c2ed134b04cd90f5e95d88590440b2
Finenyaco <593102197@qq.com>
2023-05-05 14:04:42 +0800

    add 0.0.2

---
+++ get-aliyun/0.0.2/main.tf
@@ -0,0 +1,16 @@
+data "alicloud_instances" "example" {
+  
+  status            = "Running"
+  availability_zone = "cn-shenzhen-d"
+  instance_name     = var.instance_name
+}
+
+output "instance_names" {
+  description = "A list of instances names"
+  value = "${data.alicloud_instances.example.names}"
+}
+
+output "endpoint_public_ip" {
+  description = "public ip of the ECS instance"
+  value = data.alicloud_instances.example.instances.0.public_ip
+}

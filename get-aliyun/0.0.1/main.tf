a8f921b3b36c020c3cd311a70c6b8c3e94769cc9
Finenyaco <593102197@qq.com>
2023-05-05 13:31:46 +0800

    update

--- get-aliyun/0.0.1/main.tf
+++ get-aliyun/0.0.1/main.tf
@@ -14,3 +14,8 @@ output "instance_names" {
   description = "A list of instances names"
   value = "${data.alicloud_instances.example.names}"
 }
+
+output "endpoint_public_ip" {
+  description = "public ip of the ECS instance"
+  value = data.alicloud_instances.example.instances.0.public_ip
+}

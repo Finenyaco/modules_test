8127b31676de2f0398653e87cfa3bc140f6a2bc8
Finenyaco <593102197@qq.com>
2023-05-05 16:17:05 +0800

    add 0.0.3

---
+++ aliyun-jar/0.0.3/outputs.tf
@@ -0,0 +1,14 @@
+output "endpoint_public_ip" {
+  description = "public ip of the ECS instance"
+  value = data.alicloud_instances.test_data.instances.0.public_ip
+}
+
+output "instance_names" {
+  description = "A list of instances names"
+  value = "${data.alicloud_instances.test_data.names}"
+}
+
+output "alicloud_vswitches" {
+  description = "vpc_id"
+  value = data.alicloud_vswitches.default.vpc_id
+}

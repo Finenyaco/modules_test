4fd574d8b1e6cd7a2044b14f23e74eb4ac1c2f84
Finenyaco <593102197@qq.com>
2023-05-05 13:18:54 +0800

    update

--- get-aliyun/0.0.1/main.tf
+++ get-aliyun/0.0.1/main.tf
@@ -1,4 +1,16 @@
+variable "instance_name" {
+  description = "The instance name of the ECS instance"
+  default     = "demo-instance"
+}
+
 data "alicloud_instances" "example" {
+  
   status            = "Running"
   availability_zone = "cn-shenzhen-d"
+  instance_name     = var.instance_name
+}
+
+output "instance_names" {
+  description = "A list of instances names"
+  value = "${data.alicloud_instances.example.names}"
 }
--- get-aliyun/0.0.1/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_names" {
-  description = "A list of instances names"
-  value = "${data.alicloud_instances.example.names}"
-}

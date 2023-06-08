46ab7f1b5a74458896f5bfb37600b189158f8c96
Finenyaco <593102197@qq.com>
2023-06-08 11:07:52 +0800

    remove aliyun modules to alicloud

--- get-aliyun/0.0.2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instances" {
-  description = "A list of instances"
-  value = data.alicloud_instances.example.instances
-}
-
-output "instance_ids" {
-  description = " A list of ECS instance IDs."
-  value = data.alicloud_instances.example.instances
-}

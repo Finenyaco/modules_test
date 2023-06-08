de9ab1614890ff499b5e9cef6c0c5e94715eea5d
Finenyaco <593102197@qq.com>
2023-06-08 14:49:22 +0800

    update

--- aws/aws-intance/0.0.1/main.tf
+++ aws/aws-intance/0.0.1/main.tf
@@ -1,7 +1,23 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amazon/ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+ 
+  owners = ["099720109477"]   # Canonical
+}
+
+
 # Create a VPC
 resource "aws_vpc" "test_resource" {
-  cidr_block = "172.16.0.0/16"
-
+  cidr_block = var.vpc_cidr_block
   tags = {
     Name = "test_resource"
   }
@@ -9,8 +25,8 @@ resource "aws_vpc" "test_resource" {
 
 resource "aws_subnet" "test_resource" {
   vpc_id            = aws_vpc.test_resource.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "ap-east-1a"
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
 
   tags = {
     Name = "test_resource"
@@ -19,7 +35,7 @@ resource "aws_subnet" "test_resource" {
 
 resource "aws_network_interface" "test_resource" {
   subnet_id   = aws_subnet.test_resource.id
-  private_ips = ["172.16.10.100"]
+  private_ips = var.network_interface_ip
 
   tags = {
     Name = "test_resource"
@@ -27,9 +43,8 @@ resource "aws_network_interface" "test_resource" {
 }
 
 resource "aws_instance" "test_resource" {
-  ami                  = "ami-0e0820ad173f20fbb"
-  instance_type        = "t3.micro"
-  iam_instance_profile = "AmazonSSMRoleForInstancesQuickSetup"
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = var.instance_type
   network_interface {
     network_interface_id = aws_network_interface.test_resource.id
     device_index         = 0
---
+++ aws/aws-intance/0.0.1/variables.tf
@@ -0,0 +1,38 @@
+# @label "Instance Type"
+# @group "Basic"
+variable "instance_type" {
+    type        = string
+    description = "instance type"
+    default = "t3.small"
+}
+
+# @label "Availablity Zone"
+# @group "Basic"
+variable "availability_zone" {
+    type        = string
+    description = "avaliability zone"
+    default = "ap-east-1a"
+}
+
+
+# @group "Advanced"
+variable "vpc_cidr_block" {
+    type        = string
+    description = "vpc cidr block"
+    default = "172.16.0.0/16"
+}
+
+# @group "Advanced"
+variable "subnet_cidr_block" {
+    type        = string
+    description = "subnet cidr block"
+    default = "172.16.10.0/24"
+}
+
+# @group "Advanced"
+variable "network_interface_ip" {
+    type        = string
+    description = "network interface ip"
+    default = ["172.16.10.100"]
+}
+

ad22e95d019c912a8725f7799c421ab97b112b00
Finenyaco <593102197@qq.com>
2023-06-09 14:12:10 +0800

    update

--- aws/aws-eks/0.0.1/main.tf
+++ aws/aws-eks/0.0.1/main.tf
@@ -1,6 +1,6 @@
 variable "cluster_name" {
     type = string
-    description = "Name of the cluster. Must be between 1-100 characters in length. Must begin with an alphanumeric character, and must only contain alphanumeric characters, dashes and underscores (^[0-9A-Za-z][A-Za-z0-9\-_]+$)."
+    description = "Name of the cluster. Must be between 1-100 characters in length. Must begin with an alphanumeric character, and must only contain alphanumeric characters, dashes and underscores (^[0-9A-Za-z][A-Za-z0-9-_]+$)."
     default = "test-eks"
 }
 

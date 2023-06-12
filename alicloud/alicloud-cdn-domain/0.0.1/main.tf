2576107407e2054dee55c2c39a09a441c38883e6
Finenyaco <593102197@qq.com>
2023-06-12 14:31:33 +0800

    update

--- alicloud/alicloud-cdn-domain/0.0.1/main.tf
+++ alicloud/alicloud-cdn-domain/0.0.1/main.tf
@@ -2,14 +2,33 @@ variable "name" {
   default = "*.tsfxwx.com"
 }
 
+variable "scope" {
+  type = string
+  default = "domestic"
+}
+
+variable "cdn_type" {
+  type = string
+  default = "web"
+}
+
+variable "source_type" {
+  type = string
+  default = "ipaddr"
+}
+
+variable "source_content" {
+  type = string
+  default = "1.1.1.1"
+}
 
 resource "alicloud_cdn_domain_new" "default" {
-  scope       = "domestic"
+  scope       = var.scope
   domain_name = var.name
-  cdn_type    = "web"
+  cdn_type    = var.cdn_type
   sources {
-    type     = "ipaddr"
-    content  = "1.1.1.1"
+    type     = var.source_type
+    content  = var.source_content
     priority = 20
     port     = 80
     weight   = 15
